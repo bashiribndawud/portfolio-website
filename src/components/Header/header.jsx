a10670c2e1c13737da7924762f8e01a35d9d96ad
Bashir Dauda <56880803+bashiribndawud@users.noreply.github.com>
2022-12-14 23:04:33 +0100

    header component styled

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import Cta from './cta';
+import profile from '../../assets/Bashir.png'
+import Headersocial from './headersocial';
+
 const Header = () => {
   return (
-    <div>header</div>
-  )
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Bashir Dauda</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <Cta />
+        <Headersocial />
+        <div className="me">
+          <img src={profile} alt="My Profile Pix" />
+        </div>
+
+        <a href="#contact" className="scroll__down">Scroll down</a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
