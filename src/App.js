b02ae5b4a2488e55f3deccfa79dcc9205df1fcfc
Bashir Dauda <56880803+bashiribndawud@users.noreply.github.com>
2023-07-03 23:25:51 +0100

    header done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-
 import './App.css';
-
+import Header from './components/Header/Header';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       fgdgd
-      </header>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
